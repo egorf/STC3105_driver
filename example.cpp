891329af16639435ebf63589b367ea1eed692ead
Egor Fedorov <egor.fedorov@emlid.com>
2015-03-11 22:48:00 +0000

    Prettify output of the example

--- example.cpp
+++ example.cpp
@@ -11,13 +11,17 @@ int main()
 	printf("STC connection established: %d\n", stc.testConnection());
 
 	stc.initialize();
+	printf("State-Of-Charge base = %d\n", stc.readStateOfChargeBase());
+	printf("Voltage: Current: Charge: \n");
 
+	int time = 0;
 	while (1)
 	{
 		stc.readVoltage();
 		stc.readCurrent();
-		printf("Voltage = %.3f\n", stc.getVoltage());
-		printf("Current = %.3f\n", stc.getCurrent());
-		sleep(1);
+		stc.readStateOfCharge();
+		printf("%.3f V  %.3f A  %.3f A.h %d s \n", stc.getVoltage(), stc.getCurrent(), stc.getStateOfCharge(), time);
+		time += 4;
+		sleep(4);
 	}
 }
